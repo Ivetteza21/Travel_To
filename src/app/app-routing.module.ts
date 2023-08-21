ba23c5e9135e0af3d225ffaab9c4bd45ccb692ea
Ivette Zamora <89620932+Ivetteza21@users.noreply.github.com>
2023-08-21 18:43:21 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { FirstComponent } from './first/first.component';
 import { BodyComponent } from './body/body.component';
 import { LoginComponent } from './login/login.component';
 import { CrudComponent } from './crud/crud.component';
+import { ListaAlojamientoComponent } from './lista-alojamiento/lista-alojamiento.component';
+import { AlojamientoComponent } from './alojamiento/alojamiento.component';
 
 const routes: Routes = [
   {
@@ -16,6 +18,8 @@ const routes: Routes = [
       { path: 'body', component: BodyComponent },
       { path: 'login', component: LoginComponent },
       { path: 'crud', component: CrudComponent },
+      { path: 'lista-alojamiento', component: ListaAlojamientoComponent },
+      { path: 'alojamiento', component: AlojamientoComponent },
       //{path: 'clientes',component: ClientesComponent}
     ],
   },
